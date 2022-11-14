a40e7595eefc6adc989dfc06949394855be12a0d
SANTIAGO <SANTIAGO@LAPTOP-REHH1RB2>
2022-11-13 21:24:20 -0600

    Agregar archivos de proyecto.

---
+++ ejercicio5.17/Program.cs
@@ -0,0 +1,36 @@
+﻿
+namespace Ejercicio5_17
+{
+    class Program
+    {
+        public static void Main()
+        {
+            string[] unidades = { "", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX" };
+            string[] decenas = { "", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC" };
+            string[] centenas = { "", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM" };
+            string[] miles = { "", "M", "MM", "MMM" };
+
+
+            Console.WriteLine("Ingrese el número que desea transformar a números romanos");
+
+            double numero = Convert.ToDouble(Console.ReadLine());
+            Math.Truncate(numero);
+            if (numero <= 0 || numero >= 3000)
+            {
+                Console.WriteLine("Valor no permitido");
+                return;
+            }
+
+            int m = (int)(numero / 1000);
+            int restante = (int)(numero % 1000);
+            int с = restante / 100;
+            restante = restante % 100;
+            int d = restante / 10;
+            restante = restante % 10;
+            int u = restante;
+
+            Console.WriteLine(miles.ElementAtOrDefault(m) + centenas.ElementAtOrDefault(с) + decenas.ElementAtOrDefault(d) + unidades.ElementAtOrDefault(u));
+
+        }
+    }
+}
